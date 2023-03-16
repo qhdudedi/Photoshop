478c4e476a7a985827f65b62c738430bfd0aa07c
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-03-16 22:30:20 +0900

    Update Window_Main.py

--- source code/Window_Main.py
+++ source code/Window_Main.py
@@ -53,68 +53,68 @@ class MainWindow(QMainWindow, form_class):
         self.savefile, _ = QFileDialog.getSaveFileName(self, 'save', './img', '파일(*.jpg;*.jpeg;*.png;*)')    
         cv2.imwrite(self.savefile, save_img)                       
         
-    def loadImage(self):                                        # loadImage() 불러온 편집할 이미지를 보여주는 함수 
-        loadimg = cv2.imread(self.file)                         # 파일탐색기를 통해 불러온 이미지 imread()로 읽기   
-        editimg = loadimg.copy()                                # 원본 이미지 카피를 이용한 편집할 이미지 생성
-        self.orgImg = cv2.cvtColor(editimg, cv2.COLOR_BGR2RGB)  # 원본 이미지 색 유지를 위한 cvtColor() 사용
+    def loadImage(self):                                        
+        loadimg = cv2.imread(self.file)                        
+        editimg = loadimg.copy()                                
+        self.orgImg = cv2.cvtColor(editimg, cv2.COLOR_BGR2RGB) 
 
-        self.rightimg = self.orgImg.copy()                      # 편집할 이미지 선언을 위한 원본 이미지 copy
+        self.rightimg = self.orgImg.copy()                     
         new_img = self.rightimg                                 
-        self.result = QImage(new_img.data, new_img.shape[1], new_img.shape[0], QImage.Format_RGB888)    # QImage를 통해 이미지를 불러온다.
-        self.pixmappre = QPixmap.fromImage(self.result)                                                 # 불러온 이미지 정보를 QPixmap으로 변환
-        self.edit_img.setPixmap(self.pixmappre.scaled(self.width(), self.height(), Qt.KeepAspectRatio)) # edit_img 라벨 크기에 맞춰 Pixmap 출력
+        self.result = QImage(new_img.data, new_img.shape[1], new_img.shape[0], QImage.Format_RGB888)   
+        self.pixmappre = QPixmap.fromImage(self.result)                                                 
+        self.edit_img.setPixmap(self.pixmappre.scaled(self.width(), self.height(), Qt.KeepAspectRatio)) 
 
-    def showImg(self):                                           # showImg() 불러온 기본 이미지를 보여주는 함수                   
-        self.pixmap = QPixmap(self.file)                         # 불러온 이미지 정보를 QPixmap으로 변환    
-        self.ori_img.setPixmap(self.pixmap.scaled(self.width(), self.height(), Qt.KeepAspectRatio)) # ori_img 라벨 크기에 맞춰 Pixmap 출력
+    def showImg(self):                                                              
+        self.pixmap = QPixmap(self.file)                            
+        self.ori_img.setPixmap(self.pixmap.scaled(self.width(), self.height(), Qt.KeepAspectRatio)) 
 
-    def updateImg(self):                                        # 편집한 이미지들을 바로 보여주기 위한 함수
+    def updateImg(self):                                        
         new_img = self.rightimg
-        self.result = QImage(new_img.data, new_img.shape[1], new_img.shape[0], QImage.Format_RGB888)        # QImage를 통해 이미지를 불러온다.
-        self.pixmappre = QPixmap.fromImage(self.result)                                                     # 불러온 이미지 정보를 QPixmap으로 변환
-        self.edit_img.setPixmap(self.pixmappre.scaled(self.width(), self.height(), Qt.KeepAspectRatio))     # edit_img 라벨 크기에 맞춰 Pixmap 출력
+        self.result = QImage(new_img.data, new_img.shape[1], new_img.shape[0], QImage.Format_RGB888)       
+        self.pixmappre = QPixmap.fromImage(self.result)                                                     
+        self.edit_img.setPixmap(self.pixmappre.scaled(self.width(), self.height(), Qt.KeepAspectRatio))     
     
-    def editImg(self):                                              # editImg() 이미지 편집하는 함수
+    def editImg(self):                                            
         try:                        
-            if self.btn_Blur.isChecked() == True:                   # 블러버튼이 check되었을 경우의 조건문
-                self.rightimg = self.rightimg.copy()                # 편집할 이미지 copy
-                blur_img = self.rightimg                            # 블러 효과 적용할 이미지 변수 선언
-                i = self.BlurSlider.value()                         # Slider의 움직임으로 변화되는 값 받아오는 변수 i 
-                blur = cv2.blur(blur_img, (i, i))                   # 블러링 적용 - blur():평균블러링, 커널사이즈 (i,i)로 슬라이더 움직일 때마다 적용되는 커널 사이즈 적용 
-                self.rightimg = blur                                # 적용된 블러 이미지 편집할 이미지로 지정
-                self.updateImg()                                    # updateImg()로 편집한 이미지 출력
-            if self.btn_Sharp.isChecked() == True:                  # sharp버튼이 check되었을 경우의 조건문
-                self.rightimg = self.orgImg.copy()                  # 편집할 이미지를 원본 이미지로 copy
-                shar_img = self.rightimg                            # 샤프닝 효과 적용할 이미지 변수 선언
-                j = self.SharpSlider.value()                        # slider의 움직으로 변화되는 값 받아오는 변수 j 
-                sharpenImg = np.array([[-1, -1, -1], [-1, j, -1], [-1, -1, -1]])        # 샤프닝 필터 생성
-                shar = cv2.filter2D(shar_img, -1, sharpenImg)                           # 생성한 샤프닝 필터 적용
-                self.rightimg = shar                                                    # 적용된 샤프닝 이미지 편집할 이미지로 지정
-                self.updateImg()                                                         # updateImg()로 편집한 이미지 출력
-        except:                                                     # 예외처리 문 - 프로그램 실행 실패 시 출력
+            if self.btn_Blur.isChecked() == True:                   
+                self.rightimg = self.rightimg.copy()               
+                blur_img = self.rightimg                            
+                i = self.BlurSlider.value()                         
+                blur = cv2.blur(blur_img, (i, i))                  
+                self.rightimg = blur                               
+                self.updateImg()                                    
+            if self.btn_Sharp.isChecked() == True:                  
+                self.rightimg = self.orgImg.copy()                  
+                shar_img = self.rightimg                           
+                j = self.SharpSlider.value()                       
+                sharpenImg = np.array([[-1, -1, -1], [-1, j, -1], [-1, -1, -1]])      
+                shar = cv2.filter2D(shar_img, -1, sharpenImg)                           
+                self.rightimg = shar                                                   
+                self.updateImg()                                                         
+        except:                                                     
             print("-")
 
-    def brightImg(self):                                            # brightImg 이미지 밝기 변화를 주는 함수
+    def brightImg(self):                                            
         try:
-            if self.btn_Bright.isChecked() == True:                 # 밝기 버튼 체크가 되었을 경우의 조건문
+            if self.btn_Bright.isChecked() == True:                
                 self.rightimg = self.orgImg.copy()                  
                 self.rightimg = self.rightimg.copy()
                 loadimg = self.rightimg
-                k = self.BCSlider.value()                           # slider의 움직임으로 밝기 값을 조절 
-                array = np.full(loadimg.shape,(k,k,k), dtype= np.uint8)     # 밝기 필터 생성
-                loadimg = cv2.add(loadimg,array)                            # cv2.add()함수로 두 개의 픽셀 값 더하여 출력
-                self.rightimg = loadimg                                     # 밝기 조정된 이미지 지정
-                self.updateImg()                                            # updateImg()로 편집한 이미지 출력
+                k = self.BCSlider.value()                           
+                array = np.full(loadimg.shape,(k,k,k), dtype= np.uint8)     
+                loadimg = cv2.add(loadimg,array)                           
+                self.rightimg = loadimg                                     
+                self.updateImg()                                            
             if self.btn_Bright.isChecked() == False:
                 self.updateImg()
         except:
             print("=")
-    def grayImg(self):                                                  # 이미지를 grayscale로 바꿔주는 grayImg()함수
+    def grayImg(self):                                                  
         try:
-            if self.btn_gray.isChecked() == True:                       # gray 버튼이 checked 되었을 경우 조건문
+            if self.btn_gray.isChecked() == True:                     
                 loadimg = self.rightimg
-                gray = cv2.cvtColor(loadimg, cv2.COLOR_BGR2GRAY)            # cvtColor()함수를 통해 3채널에서 단채널로 변환된다.
-                editimg = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)            # cvtColor()함수를 통하여 이미지포멧을 위한 3채널로 변경.
+                gray = cv2.cvtColor(loadimg, cv2.COLOR_BGR2GRAY)            
+                editimg = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)            
                 self.rightimg = editimg
                 self.updateImg()                                            # updateImg()로 편집한 이미지 출력
             if self.btn_original.isChecked() == True:                   # original 버튼이 활성화되었을 경우
